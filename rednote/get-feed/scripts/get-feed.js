595ef7963fcbee1e4cf60e3b84b580ad1b533919
firewox <1360033708@qq.com>
2026-03-31 01:04:24 +0800

    Fix: Use SystemBrowserManager for get-feed skill

--- rednote/get-feed/scripts/get-feed.js
+++ rednote/get-feed/scripts/get-feed.js
@@ -3,7 +3,7 @@
 const path = require('path');
 
 const libPath = path.join(__dirname, '..', '..', 'lib');
-const { getBrowserManager, closeAll } = require(path.join(libPath, 'browser'));
+const { SystemBrowserManager } = require(path.join(libPath, 'system-browser'));
 const { logger, randomSleep } = require(path.join(libPath, 'utils'));
 
 const DATA_PATH = process.env.XHS_DATA_PATH || path.join(__dirname, '..', '..', 'data');
@@ -28,7 +28,7 @@ async function getFeed(params) {
   
   logger.info(`获取帖子详情: ${targetNoteId}`);
   
-  const browserManager = getBrowserManager(dataPath, { headless: false });
+  const browserManager = new SystemBrowserManager({ headless: false });
   
   try {
     const page = await browserManager.getPage();
